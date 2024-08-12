fea64b27469b3f3a1bac31719e34f6d6145c4926
Mostafa-Mahmoud4 <167915901+Mostafa-Mahmoud4@users.noreply.github.com>
2024-08-12 17:47:39 +0300

    Updated jobController.js

--- job-search-app/controllers/jobController.js
+++ job-search-app/controllers/jobController.js
@@ -1,6 +1,12 @@
 const Job = require('../models/jobModel');
 const Application = require('../models/applicationModel');
 
+/**
+ * Adds a new job.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
 const addJob = async (req, res, next) => {
     const { jobTitle, jobLocation, workingTime, seniorityLevel, jobDescription, technicalSkills, softSkills, companyId } = req.body;
     const addedBy = req.user._id;
@@ -19,6 +25,12 @@ const addJob = async (req, res, next) => {
     res.status(201).json({ message: 'Job added successfully', id: job._id, job });
 };
 
+/**
+ * Updates an existing job.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
 const updateJob = async (req, res, next) => {
     const { id } = req.params;
     const updateData = req.body;
@@ -26,24 +38,48 @@ const updateJob = async (req, res, next) => {
     res.status(200).json({ message: 'Job updated successfully', job });
 };
 
+/**
+ * Deletes a job.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
 const deleteJob = async (req, res, next) => {
     const { id } = req.params;
     await Job.findByIdAndDelete(id);
     res.status(200).json({ message: 'Job deleted successfully' });
 };
 
+/**
+ * Retrieves all jobs.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
 const getAllJobs = async (req, res, next) => {
     const jobs = await Job.find().populate('companyId');
     res.status(200).json({ jobs });
 };
 
+/**
+ * Retrieves all jobs for a specific company.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
 const getJobsByCompany = async (req, res, next) => {
     const { companyId } = req.params;
-    console.log(companyId)
+    console.log(companyId);
     const jobs = await Job.find({ companyId }).populate('addedBy');
     res.status(200).json({ jobs });
 };
 
+/**
+ * Retrieves jobs based on various filters.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
 const getFilteredJobs = async (req, res, next) => {
     const { workingTime, jobLocation, seniorityLevel, jobTitle, technicalSkills } = req.query;
     const filter = {};
@@ -56,10 +92,16 @@ const getFilteredJobs = async (req, res, next) => {
     res.status(200).json({ jobs });
 };
 
+/**
+ * Submits an application to a job.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
 const applyToJob = async (req, res, next) => {
-    const { jobId,  userTechSkills, userSoftSkills, userResume } = req.body;
+    const { jobId, userTechSkills, userSoftSkills, userResume } = req.body;
     const userId = req.user._id;
-    console.log(userId)
+    console.log(userId);
     const application = new Application({
         jobId,
         userId,
