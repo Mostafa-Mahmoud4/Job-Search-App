6b7498bd3e3c6601a05fd7153e912cec5ab79420
Mostafa-Mahmoud4 <167915901+Mostafa-Mahmoud4@users.noreply.github.com>
2024-08-12 17:45:29 +0300

    Updated companyController.js

--- job-search-app/controllers/companyController.js
+++ job-search-app/controllers/companyController.js
@@ -3,6 +3,12 @@ const User = require('../models/userModel');
 const Job = require('../models/jobModel');
 const Application = require('../models/applicationModel');
 
+/**
+ * Adds a new company.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
 const addCompany = async (req, res, next) => {
     const { companyName, description, industry, address, numberOfEmployees, companyEmail } = req.body;
     const companyHR = req.user._id;
@@ -19,6 +25,12 @@ const addCompany = async (req, res, next) => {
     res.status(201).json({ message: 'Company added successfully', company });
 };
 
+/**
+ * Updates an existing company.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
 const updateCompany = async (req, res, next) => {
     const { id } = req.params;
     const updateData = req.body;
@@ -26,20 +38,35 @@ const updateCompany = async (req, res, next) => {
     res.status(200).json({ message: 'Company updated successfully', company });
 };
 
+/**
+ * Deletes a company.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
 const deleteCompany = async (req, res, next) => {
     const { id } = req.params;
     await Company.findByIdAndDelete(id);
     res.status(200).json({ message: 'Company deleted successfully' });
 };
 
+/**
+ * Retrieves a company by its ID.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ */
 const getCompany = async (req, res, next) => {
     const { id } = req.params;
     const company = await Company.findById(id).populate('companyHR');
     res.status(200).json({ company });
 };
 
-
-
+/**
+ * Searches for companies by name.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ */
 const searchCompany = async (req, res) => {
     const { name } = req.query;
 
@@ -54,6 +81,11 @@ const searchCompany = async (req, res) => {
     res.status(200).json(companies);
 };
 
+/**
+ * Retrieves all applications for a specific job.
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ */
 const getApplicationsForJob = async (req, res) => {
     const { jobId } = req.params;
     const applications = await Application.find({ jobId }).populate('userId');
